d11f1819c4a7d1f77dbcdb2bee49153e9f4952eb
Broderick Westrope <broderickwestrope@gmail.com>
2024-03-15 11:38:15 +1100

    feat: add Astro Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        UnoCSS({
-            injectReset: true,
-        }),
-    ],
+  integrations: [UnoCSS({
+    injectReset: true
+  }),
+  vue(),
+  ],
 });

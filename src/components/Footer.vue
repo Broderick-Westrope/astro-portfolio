801cfe0381de2f2192b0abe025822edd24a2a5e3
Broderick Westrope <broderickwestrope@gmail.com>
2024-09-06 12:44:37 +1000

    fix: footer colors

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,22 +4,22 @@ import { getLinkTarget } from '@/utils/link'
 </script>
 
 <template>
-  <footer class="w-full mt-20 pt-6 pb-8 max-w-3xl text-sm flex flex-col gap-4 border-t !border-opacity-50 text-dark dark:text-white">
+  <footer class="w-full mt-20 pt-6 pb-8 max-w-3xl text-sm flex flex-col gap-4 border-t !border-opacity-50">
     <div v-if="siteConfig.footer.navLinks && siteConfig.footer.navLinks.length > 0" class="flex flex-wrap gap-4">
       <template v-for="(link, index) in siteConfig.footer.navLinks" :key="link.text">
-        <a
-          :aria-label="`${link.text}`" :target="getLinkTarget(link.href)" class="opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer flex items-center"
-          :href="link.href"
-        >
+        <a :aria-label="`${link.text}`" :target="getLinkTarget(link.href)"
+          class="hover:text-primary transition-colors duration-200 cursor-pointer flex items-center" :href="link.href">
           {{ link.text }}
         </a>
         <span v-if="index < siteConfig.footer.navLinks.length - 1" class="opacity-70"> / </span>
       </template>
     </div>
     <div class="flex">
-      <a class="opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
-      <span class="opacity-70">&nbsp;&nbsp;&copy;&nbsp;&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;{{ siteConfig.author
-      }}.</span>
+      <a class="hover:text-primary transition-colors duration-200 cursor-pointer"
+        href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>
+      <span>&nbsp;&nbsp;&copy;&nbsp;&nbsp;{{ new Date().getFullYear() }}&nbsp;&nbsp;{{
+      siteConfig.author
+        }}.</span>
     </div>
   </footer>
 </template>
--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -6,9 +6,9 @@ const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-<label class="swap swap-rotate">
-  <input id="theme-controller" type="checkbox" class="theme-controller" value="light" @click="toggleDark()" />
-  <i class="iconify akar-icons--sun size-6 swap-on" />
-  <i class="iconify akar-icons--moon size-6 swap-off" />
-</label>
+  <label class="swap swap-rotate hover:text-primary">
+    <input id="theme-controller" type="checkbox" class="theme-controller" value="light" @click="toggleDark()" />
+    <i class="iconify akar-icons--sun size-6 swap-on" />
+    <i class="iconify akar-icons--moon size-6 swap-off" />
+  </label>
 </template>

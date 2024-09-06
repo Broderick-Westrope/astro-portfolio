89caa0caf04bb0cc608a8e05309d183932838226
Broderick Westrope <broderickwestrope@gmail.com>
2024-09-06 10:08:30 +1000

    refactor: cleanup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -81,12 +81,10 @@ function toggleNavDrawer() {
 
 <template>
   <header id="header" :class="{ 'backdrop-blur-sm': scroll > 20 }"
-    class="!fixed bg-transparent z-50 w-screen h-20 px-6 flex justify-between items-center">
+    class="!fixed bg-transparent z-50 w-full h-20 px-6 flex justify-between items-center">
     <div class="flex items-center h-full gap-x-6">
-      <a href="/" mr-6 aria-label="Header Logo Image">
-        <img width="32" height="32" :src="siteConfig.header.logo.src" :alt="siteConfig.header.logo.alt">
-      </a>
-      <nav class="sm:flex hidden flex-wrap gap-x-6 position-initial flex-row">
+      <a href="/" mr-6 aria-label="Header Logo Image" class="iconify akar-icons--sparkles size-8" />
+      <nav class="sm:flex items-center justify-center hidden flex-wrap gap-x-6 position-initial flex-row">
         <a v-for="link in navLinks" :key="link.text" :aria-label="`${link.text}`" :target="getLinkTarget(link.href)"
           class="opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer" :href="link.href">
           {{ link.text }}
@@ -97,19 +95,25 @@ function toggleNavDrawer() {
       </div>
     </div>
     <div class="flex gap-x-6">
-      <a v-for="link in socialLinks" :key="link.text" :aria-label="`${link.text}`" :class="link.icon" class="iconify opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer"
+      <a v-for="link in socialLinks" :key="link.text" :aria-label="`${link.text}`" :class="link.icon"
+        class="iconify hover:text-primary transition-colors duration-200 cursor-pointer size-6"
         :target="getLinkTarget(link.href)" :href="link.href" />
 
-      <a class="opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer" target="_blank" href="/rss.xml" i-ri-rss-line aria-label="RSS" />
+      <a class="hover:text-primary transition-colors duration-200 cursor-pointer iconify akar-icons--rss size-6"
+        target="_blank" href="/rss.xml" aria-label="RSS" />
       <ThemeToggle />
     </div>
   </header>
-  <nav id="nav-drawer" class="-translate-x-full sm:hidden box-border fixed h-screen z-50 left-0 top-0 min-w-32 max-w-60 p-6 text-lg flex flex-col gap-5 transition-all bg-base-200">
-    <span class="iconify akar-icons--three-line-horizontal size-6" />
+  <nav id="nav-drawer"
+    class="-translate-x-full sm:hidden box-border fixed h-screen z-50 left-0 top-0 w-1/2 p-6 text-lg flex flex-col gap-5 transition-all bg-base-200">
+    <span class="iconify akar-icons--arrow-left size-6" @click="toggleNavDrawer()" />
     <a v-for="link in navLinks" :key="link.text" :aria-label="`${link.text}`" :target="getLinkTarget(link.href)"
-      class="opacity-70 hover:opacity-100 transition-opacity duration-200 cursor-pointer" :href="link.href" @click="toggleNavDrawer()">
+      class="hover:text-primary transition-colors duration-200 cursor-pointer" :href="link.href"
+      @click="toggleNavDrawer()">
       {{ link.text }}
     </a>
   </nav>
-  <div id="nav-drawer-mask" class="hidden transition-all content-none fixed top-0 left-0 w-full h-full bg-base-300 z-40 opacity-50" @click="toggleNavDrawer()" />
+  <div id="nav-drawer-mask"
+    class="hidden transition-all content-none fixed top-0 left-0 w-full h-full bg-base-300 z-40 opacity-50"
+    @click="toggleNavDrawer()" />
 </template>
--- src/components/ScrollToTop.vue
+++ src/components/ScrollToTop.vue
@@ -12,12 +12,9 @@ function toTop() {
 </script>
 
 <template>
-  <button
-    aria-label="Scroll to top"
-    class="fixed right-5, sm:right-32 bottom-32 w-12 h-12 text-lg hover:opacity-100 rounded-full flex items-center justify-center bg-accent text-accent-content transition duration-300 z-10 print:hidden"
-    :class="scroll > 300 ? 'opacity-75' : 'opacity-0 pointer-events-none'"
-    @click="toTop()"
-  >
+  <button aria-label="Scroll to top"
+    class="fixed right-5, sm:right-32 bottom-32 size-12 text-lg hover:opacity-100 rounded-full flex items-center justify-center bg-accent text-accent-content transition duration-300 z-10 print:hidden"
+    :class="scroll > 300 ? 'opacity-75' : 'opacity-0 pointer-events-none'" @click="toTop()">
     <span class="iconify akar-icons--arrow-up" />
   </button>
 </template>

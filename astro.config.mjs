5feb0990d804bf51b29ca7078e646e69a1594d6b
Broderick Westrope <broderickwestrope@gmail.com>
2024-09-08 18:24:09 +1000

    chore: setup svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://westrope.dev/',
-    integrations: [
-        mdx(),
-        sitemap(),
-        vue(),
-        tailwind(),
-    ],
+    integrations: [mdx(), sitemap(), vue(), tailwind(), svelte()],
     markdown: {
         shikiConfig: {
             themes: {

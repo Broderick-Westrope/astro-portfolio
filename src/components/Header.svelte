da38a844157ede4e085f9b4cb106a286fa977557
Broderick Westrope <broderickwestrope@gmail.com>
2024-09-10 19:19:33 +1000

    chore: disable broken ThemeController component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,7 +2,6 @@
   import { onMount } from "svelte";
   import siteConfig from "@/site-config";
   import { getLinkTarget } from "@/utils/link";
-  import ThemeController from "./ThemeController.svelte";
 
   let scroll = 0;
   let oldScroll = 0;
@@ -126,7 +125,7 @@
       href="/rss.xml"
       aria-label="RSS"
     />
-    <ThemeController />
+    <!-- <ThemeController /> -->
   </div>
 </header>
 <nav
